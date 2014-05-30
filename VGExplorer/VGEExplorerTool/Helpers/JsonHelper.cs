cfbff26efe01e38405ec395d6f30095430d63b25
José Cordero <jcorderodr@users.noreply.github.com>
2014-05-30 19:46:59 -0400

    - add Win32 App - add Newtonsoft.JSON package - Web (now empty) project for a JavaScript viewer
    PENDING:
    - UI improvement
    - Icons

---
+++ VGExplorer/VGEExplorerTool/Helpers/JsonHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using VGEExplorerTool.Entities;
+
+namespace VGEExplorerTool.Helpers
+{
+    public class JsonHelper
+    {
+
+        public static String Serialize(NodeString nodeString)
+        {
+            return JsonConvert.SerializeObject(nodeString, Formatting.Indented);
+        }
+
+        public static NodeString Deserialize(String nodeStringContent)
+        {
+            return JsonConvert.DeserializeObject<NodeString>(nodeStringContent);
+        }
+
+    }
+
+}
